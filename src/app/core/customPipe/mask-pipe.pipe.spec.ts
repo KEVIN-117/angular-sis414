29e4a6f3f7a9c938a50dd4219e69d93217a8bd5c
Kevin Alexis Roodriguez Condori <krodriguezcondori34@gmai.com>
2024-05-23 17:01:27 -0400

    added branch clase-2

--- src/app/core/customPipe/mask-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MaskPipePipe } from './mask-pipe.pipe';
-
-describe('MaskPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new MaskPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
