c81fa63066c4f20ac80f070d0ffb5cb4976860ee
Kevin Alexis Roodriguez Condori <krodriguezcondori34@gmai.com>
2024-05-27 07:26:38 -0400

    angular auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import {provideHttpClient} from "@angular/common/http";
-import { DragonballService } from './core/services/dragonball.service';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), DragonballService]
+  providers: [provideRouter(routes), provideClientHydration()]
 };
--- src/environments/environment.development.ts
+++
@@ -1,3 +0,0 @@
-export const environmentDevelopment = {
-  urlBase : 'https://dragonball-api.com/api/characters',
-}
